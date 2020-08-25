f748f4214179719649b3f2a59800cdf7f9814026
Linuxkay <sccck000@gmail.com>
2020-08-25 21:23:43 +0900

    new file added

---
+++ lecture50.sql
@@ -0,0 +1,4 @@
+# get data id, last_name, gender male and limit showing 10
+select id, last_name, gender from users where gender = 1 limit 10;
+# get data id, last_name, gender female and limit showing 10
+select id, last_name, gender from users where gender =2 limit 10;
