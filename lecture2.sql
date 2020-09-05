d86a3181becdeb412fe7ab2b47d6713034183bf4
Linuxkay <sccck000@gmail.com>
2020-09-06 07:48:00 +0900

    lecture2.sql updated

--- lecture2.sql
+++ lecture2.sql
@@ -4,5 +4,5 @@ select * from users where last_name like '中%';
 select * from users where last_name like '%中%';
 # search word ends by 子
 select * from users where first_name like '%子';
-# search word ends by 子 but 3chars.
+# search word ends by 子 but 3chars on top.
 select * from users where first_name like '__子';
